2ed693cb91a2bb74b447931326a05e02ce638144
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-15 22:34:50 -0300

    feat(Autor)
    - Adicionando atributo Apresentacao.

--- src/avaliador/src/main/java/avaliador/model/Autor.java
+++ src/avaliador/src/main/java/avaliador/model/Autor.java
@@ -4,6 +4,7 @@ public class Autor extends Entidade {
 	private String nome;
 	private String email;
 	private String genero;
+	private Apresentacao apresentacao;
 
 	public Autor() {
 		
@@ -31,5 +32,13 @@ public class Autor extends Entidade {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Apresentacao getApresentacao() {
+		return apresentacao;
+	}
+
+	public void setApresentacao(Apresentacao apresentacao) {
+		this.apresentacao = apresentacao;
 	}
 }
