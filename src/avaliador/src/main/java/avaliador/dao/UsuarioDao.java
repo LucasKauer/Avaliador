598074390ebac92634c5f444b1228d7c16b23ede
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-16 15:53:01 -0300

    feat(UsuarioDao)
    - Alteração no método de cadastro do usuário
    - Criação do método que valida o login

--- src/avaliador/src/main/java/avaliador/dao/UsuarioDao.java
+++ src/avaliador/src/main/java/avaliador/dao/UsuarioDao.java
@@ -14,7 +14,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import avaliador.model.NivelUsuario;
-import avaliador.model.SenhaHash;
 import avaliador.model.Usuario;
 
 @Component
@@ -23,14 +22,33 @@ public class UsuarioDao {
 	@Inject
 	private JdbcTemplate jdbcTemplate;
 	
+	private static final String COMANDO_SQL_INSERT_USUARIO = "INSERT INTO Usuario (Login, Senha, Nivel_Usuario) VALUES (?, ?, ?)";
+	private static final String COMANDO_SQL_SELECT_USUARIO = "SELECT Id_Usuario,  FROM Usuario WHERE Login = (?) and Senha = (?)";
+	
 	// Adiciona um usuario no banco
 	public void inserirUsuario(Usuario usuario) throws DataAccessException, NoSuchAlgorithmException, InvalidKeySpecException {
-		
-		String senhaCriptografada = SenhaHash.criaHash(usuario.getSenha());
-		
 		jdbcTemplate.update(
-				"INSERT INTO USUARIO (LOGIN, SENHA, NIVEL_USUARIO) VALUES (?, ?, ?)",
-				usuario.getLogin(), senhaCriptografada, NivelUsuario.COMUM);
+				COMANDO_SQL_INSERT_USUARIO,
+				usuario.getLogin(),
+				usuario.getSenha(),
+				usuario.getTipoUsuario());
 	}
-
+	
+	//Valida o usuario quando ele faz o login
+	public Usuario validarUsuario(String login, String senha){
+		List<Usuario> usuariosEncontrados = jdbcTemplate.query(COMANDO_SQL_SELECT_USUARIO, new RowMapper<Usuario>(){
+			
+			@Override
+			public Usuario mapRow(ResultSet rs, int arg1) throws SQLException {
+				Usuario usuario = new Usuario();
+				usuario.setId(rs.getInt("Id_Usuario"));
+				usuario.setLogin(rs.getString("Login"));
+				usuario.setSenha(rs.getString("Senha"));
+				usuario.setTipoUsuario(NivelUsuario.valueOf(rs.getString("Nivel_Usuario")));
+				return usuario;
+			}		
+		}, login, senha);
+		
+		return usuariosEncontrados.isEmpty() ? null : usuariosEncontrados.get(0); 
+	}
 }
