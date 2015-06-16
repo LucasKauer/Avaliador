f0fd896d05a906888e4fc22d965eda0f2c55b31b
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-16 17:25:23 -0300

    create(Session)
    - Arquivos necessários para sessão

---
+++ src/avaliador/src/main/java/avaliador/MvcConfig.java
@@ -0,0 +1,16 @@
+package avaliador;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import avaliador.controller.AutorizadorInterceptor;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AutorizadorInterceptor());
+	}	
+
+}
