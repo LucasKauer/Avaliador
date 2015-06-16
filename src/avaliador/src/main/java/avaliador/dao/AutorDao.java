d9bb50e0e0048788113a567553a918e4e1cf776b
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-15 22:38:28 -0300

    feat(AutorDao)
    - Adicionando métodos de verificação de autor existente
    - Chmando métodos de inserção, edição e deletar da classe AutorApresentacaoDao

--- src/avaliador/src/main/java/avaliador/dao/AutorDao.java
+++ src/avaliador/src/main/java/avaliador/dao/AutorDao.java
@@ -1,38 +1,64 @@
 package avaliador.dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import avaliador.model.Apresentacao;
 import avaliador.model.Autor;
-import avaliador.model.Categoria;
-import avaliador.model.Situacao;
+
 
 @Component
 public class AutorDao {
 	
 	@Inject
 	private JdbcTemplate jdbcTemplate;
+	AutorApresentacaoDao apresentacaoAutor  = new AutorApresentacaoDao();
 	
 	private static final String COMANDO_SQL_SELECT = "SELECT Id_Autor, Nome, Genero, Email FROM Autor";
 	private static final String COMANDO_SQL_INSERT = "INSERT INTO Autor(Nome, Genero, Email) values (?, ?, ?)";
 	private static final String COMANDO_SQL_UPDATE = "UPDATE Autor SET Nome = ?, Genero = ?, Email = ?  WHERE Id_Autor = ?";
 	private static final String COMANDO_SQL_DELETE = "DELETE FROM Autor WHERE Id_Autor = ?";
+	private static final String COMANDO_SQL_VERIFICA_AUTOR = "SELECT Id_Autor, Nome FROM Autor WHERE Id_Autor = ? OR Nome = ?";
 	
 	/**
 	 * Adiciona um autor no banco
 	 * @param autor Autor a ser inserida no banco
 	 */
 	public void inserirAutor(Autor autor) {
-		jdbcTemplate.update(COMANDO_SQL_INSERT,
-				autor.getNome(),
-				autor.getGenero(),
-				autor.getEmail());
+		List<Autor> autores = verificaAutor(autor.getId(), autor.getNome());
+		if (autores.size() == 0) {
+			jdbcTemplate.update(COMANDO_SQL_INSERT,
+					autor.getNome(),
+					autor.getGenero(),
+					autor.getEmail());
+		apresentacaoAutor.inserirAutorApresentacao(autor.getApresentacao(), autor.getId());
+		}
+	}
+	
+	/**
+	 * Verifica se já existe um autor no banco
+	 * @param idAutor para buscar no banco
+	 * @param nomeAutor para buscar no banco
+	 */
+	private List<Autor> verificaAutor(int idAutor, String nomeAutor){
+		List<Autor> autores = jdbcTemplate.query(COMANDO_SQL_VERIFICA_AUTOR, new RowMapper<Autor>(){
+			@Override
+			public Autor mapRow(ResultSet rs, int arg1) throws SQLException{
+				Autor autor = new Autor();
+				autor.setId(rs.getInt("Id_Autor"));
+				autor.setNome(rs.getString("Nome"));
+				return autor; 	
+			}
+		},idAutor, nomeAutor);
+		
+		return autores;
+
 	}
 	
 	/**
@@ -46,15 +72,17 @@ public class AutorDao {
 				autor.getGenero(),
 				autor.getEmail(),
 				id);
+		apresentacaoAutor.alteraAutorApresentacao(autor.getApresentacao(), id);
 	}
 	
 	/**
 	 * Apaga um autor do banco
 	 * @param id Id do autor a ser excluido
 	 */
-	public void deletaAutor(int id) {
+	public void deletaAutor(int id, Autor autor) {
 		jdbcTemplate.update(COMANDO_SQL_DELETE,
 				id);
+		apresentacaoAutor.deletaAutorApresentacao(autor.getApresentacao(), id);
 	}
 	
 	/**
