eefa70d3fa3e6a1aa737c4948e6c41dda7c1ca6a
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-16 02:58:27 -0300

    feat(Categoria)
    - Adição de categorias de exemplo

--- src/avaliador/src/main/java/avaliador/model/Categoria.java
+++ src/avaliador/src/main/java/avaliador/model/Categoria.java
@@ -1,5 +1,19 @@
 package avaliador.model;
 
 public enum Categoria {
+	TRABALHO_CIENTIFICO("Trabalho Científico"), POWER_POINT("Power Point"), TRABALHO_DE_CONCLUSAO("Trabalho de Conclusão");
 	
+	private String descricao;
+	
+	private Categoria(String descricao) {
+		this.setDescricao(descricao);
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	private void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
