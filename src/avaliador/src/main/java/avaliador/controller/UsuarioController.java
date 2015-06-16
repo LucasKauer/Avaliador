75e3d9d9be4b9091f9b9473cfd591cb93adae01e
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-16 15:51:38 -0300

    feat(Usuariocontroller)
    - Método que valida login

--- src/avaliador/src/main/java/avaliador/controller/UsuarioController.java
+++ src/avaliador/src/main/java/avaliador/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package avaliador.controller;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -7,12 +8,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import avaliador.dao.UsuarioDao;
+import avaliador.model.NivelUsuario;
+import avaliador.model.Usuario;
+
 @Controller
 public class UsuarioController {
+	
+	@Inject
+	UsuarioDao dao;
 	
 	@RequestMapping(value = "/entrar", method = RequestMethod.GET)
 	public String entrar(Model model, HttpSession session) {
 		return "login";
+	}
+	
+	@RequestMapping(value = "/valida", method = RequestMethod.POST)
+	public String validarUsuario(Usuario usuarioRecebido) {
+		Usuario usuarioRetornado = dao.validarUsuario(usuarioRecebido.getLogin(), usuarioRecebido.getSenha());
+		if(usuarioRetornado!=null){
+			if (usuarioRetornado.getTipoUsuario() == NivelUsuario.ADMINISTRADOR){
+				return "";
+			}
+			else{
+				return "";
+			}
+		}else{
+			return "login";
+		}
 	}
 	
 	@RequestMapping(value = "/cadastrar-usuario", method = RequestMethod.GET)
