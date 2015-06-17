87715ba78ebd5538009980bae3a1c27b9af15984
Gabriela de Souza <gabrieladessouza@gmail.com>
2015-06-17 17:44:29 -0300

    create (popularBanco.sql)

---
+++ src/popularBanco/popularBanco.sql
@@ -0,0 +1,77 @@
+
+-- Insert de Apresentacao
+
+INSERT INTO Apresentacao (Id_Apresentacao, Titulo, Resumo, Categoria, Situacao) VALUES
+(1, "CWI Crescer", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dignissim tellus vel nunc semper feugiat vitae sit amet libero. Nulla a neque faucibus, vehicula lectus quis, eleifend nisl. In sed risus aliquet, dignissim augue id, ullamcorper quam. Praesent mauris erat, viverra eget lectus at, fringilla eleifend libero. Proin sit amet elit vitae dolor lacinia tincidunt. Sed venenatis augue ac ipsum ultricies, at aliquam ligula blandit. Duis porta sollicitudin nisi id bibendum. Duis at arcu massa.", "Trabalho de Conclusão", "19/05/2015", "true"),
+(2, "Filmator Web", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut libero non dui pharetra pretium. Nulla sit amet condimentum sem. Vivamus ut rhoncus orci, sed ullamcorper neque. Duis venenatis, orci ut ultrices sodales, purus lorem pharetra augue, vitae porta nisl dolor et lacus. Maecenas ac purus eu urna tempor placerat non dapibus est. Proin pharetra dui sagittis tellus auctor pulvinar. Etiam nec ex viverra, elementum nunc ut, semper nunc. Mauris pulvinar accumsan rutrum. Donec lacus sapien, convallis vitae massa a, vulputate aliquam lorem. Suspendisse a tincidunt risus. Cras imperdiet id enim convallis ultricies. Curabitur vitae laoreet mauris, in tempor est. Sed sit amet quam vitae enim sodales semper. Aliquam in nisl id nisl egestas maximus ut a mi. Nulla facilisi. Curabitur feugiat in erat ultrices iaculis. Etiam molestie ut magna id euismod. Sed quis diam ornare sapien ultricies egestas. Suspendisse vel sapien semper, suscipit urna in, imperdiet nisi. Morbi finibus dui vel iaculis scelerisque. Ut a elementum orci. Vestibulum accumsan, lectus ut vulputate euismod, lectus tortor eleifend diam, vitae luctus libero arcu vitae justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed a lectus felis.", "Power Point", "20/06/2016", "true"),
+(3, "Turbo Rango", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a dolor ligula. Vivamus vehicula efficitur mi eget interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris eget sodales tortor. Proin dictum massa lectus, vel faucibus neque aliquam accumsan. Nulla lacinia feugiat felis vel convallis. Vestibulum turpis justo, eleifend et magna eget, consequat accumsan massa. Fusce in efficitur eros. Aenean eget arcu semper, finibus ipsum vel, porta augue. Pellentesque pellentesque dapibus neque vel vehicula. Curabitur ut dignissim nibh, non gravida lorem. Vestibulum sodales purus orci, vel efficitur massa consequat sodales. Cras massa lectus, molestie sit amet ipsum eu, volutpat consectetur nisl. Aenean ante diam, vulputate vitae urna eget, accumsan convallis erat. Donec fermentum ultricies risus et efficitur. Suspendisse porttitor ac ipsum eu sodales. Vestibulum quis augue quis diam aliquam dignissim. Morbi pretium dui ac turpis pharetra, non bibendum augue hendrerit. Nunc nunc ipsum, tincidunt eget dictum non, rutrum dapibus ipsum. Sed vehicula non ex id condimentum. Quisque ornare consectetur eros, id congue mauris consectetur ac.", "Trabalho Científico", "23/06/2015", "false"),
+(4, "Apresentator", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida consectetur urna ac cursus. In hac habitasse platea dictumst. Aenean commodo eget tortor id ultricies. Etiam dui mi, viverra laoreet condimentum quis, sollicitudin sit amet est. Ut et elit et arcu euismod interdum sed in nibh. Morbi elementum ut metus vitae consectetur. Vivamus blandit velit tellus, eget iaculis ligula lobortis eget. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Morbi egestas sollicitudin velit, quis viverra lectus cursus sed. Curabitur a elementum ligula. Integer tempus turpis lorem, id porta dolor venenatis vel. Mauris efficitur, quam ac efficitur aliquam, lacus risus facilisis magna, at faucibus dui arcu vel tortor. Suspendisse ut pharetra elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc faucibus tempor urna et placerat. Mauris et tempor mauris, non cursus justo. Curabitur pulvinar nunc velit, et mollis enim malesuada eget. In fermentum a mauris vulputate venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+In ex turpis, bibendum id nulla non, ultricies molestie quam. Nam ex orci, tincidunt ut justo mattis, egestas consequat purus. Vestibulum a euismod dolor. Proin condimentum elit vel libero sodales tristique. Cras sit amet consequat enim, sed tempor nunc. Praesent sed venenatis augue. Etiam mattis dui in fringilla sodales.
+Nulla facilisi. Duis sodales imperdiet enim sit amet pulvinar. Aenean orci elit, vestibulum eu diam in, dapibus vulputate purus. Integer feugiat, eros eget posuere dapibus, odio elit gravida purus, a sodales arcu justo non massa. Ut scelerisque, libero vitae porta gravida, sapien purus tincidunt tellus, eget tristique lacus lorem vitae odio. Phasellus rutrum venenatis pulvinar. Vivamus odio turpis, gravida eget nisl et, eleifend laoreet elit.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida consectetur urna ac cursus. In hac habitasse platea dictumst. Aenean commodo eget tortor id ultricies. Etiam dui mi, viverra laoreet condimentum quis, sollicitudin sit amet est. Ut et elit et arcu euismod interdum sed in nibh. Morbi elementum ut metus vitae consectetur. Vivamus blandit velit tellus, eget iaculis ligula lobortis eget. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Morbi egestas sollicitudin velit, quis viverra lectus cursus sed. Curabitur a elementum ligula. Integer tempus turpis lorem, id porta dolor venenatis vel. Mauris efficitur, quam ac efficitur aliquam, lacus risus facilisis magna, at faucibus dui arcu vel tortor. Suspendisse ut pharetra elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc faucibus tempor urna et placerat. Mauris et tempor mauris, non cursus justo. Curabitur pulvinar nunc velit, et mollis enim malesuada eget. In fermentum a mauris vulputate venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+In ex turpis, bibendum id nulla non, ultricies molestie quam. Nam ex orci, tincidunt ut justo mattis, egestas consequat purus. Vestibulum a euismod dolor. Proin condimentum elit vel libero sodales tristique. Cras sit amet consequat enim, sed tempor nunc. Praesent sed venenatis augue. Etiam mattis dui in fringilla sodales.
+Nulla facilisi. Duis sodales imperdiet enim sit amet pulvinar. Aenean orci elit, vestibulum eu diam in, dapibus vulputate purus. Integer feugiat, eros eget posuere dapibus, odio elit gravida purus, a sodales arcu justo non massa. Ut scelerisque, libero vitae porta gravida, sapien purus tincidunt tellus, eget tristique lacus lorem vitae odio. Phasellus rutrum venenatis pulvinar. Vivamus odio turpis, gravida eget nisl et, eleifend laoreet elit.
+In ex turpis, bibendum id nulla non, ultricies molestie quam. Nam ex orci, tincidunt ut justo mattis, egestas consequat purus. Vivamus odio turpis, gravida eget nisl et, eleifend laoreet elit. Nulla facilisi. Duis sodales.
+", "Trabalho de Conclusão", "20/06/2015", "false");
+
+-- Insert Autor
+INSERT INTO Autor (Id_Autor, Nome, Genero) VALUES 
+(1, "Lucas Kauer", "lucaskauer@gmail.com"),
+(2, "Rodrigo Molina", "rodrigomolina@hotmail.com"),
+(3, "Pablo Diehl", "pablodiehl@yahoo.com"),
+(4, "Gabriela de Souza", "gabrielasouza@gmail.com"),
+(5, "Paula Tomasi", "paulatomasi@hotmail.com"),
+(6, "Juliano Freitas", "julianofreitas@yahoo.com");
+
+-- Insert Autor_Apresentacao
+INSERT INTO Autor_Apresentacao (Id_Autor_Apresentacao, Apresentacao_Id, Autor_Id) VALUES
+(1, 1, 1),
+(1, 1, 2),
+(1, 1, 3),
+
+(2, 3, 4),
+(2, 3, 5),
+(2, 3, 6),
+(2, 3, 1),
+
+(3, 4, 2),
+(3, 4, 5),
+
+(4, 2, 4);
+
+-- Insert Avaliador
+INSERT INTO Avaliador (Id_Avaliador, Instituicao, Nome, Idade, Genero, Usuario_id) VALUES
+(1, "Feevale", "Mariane Ferreira", 25),
+(2, "IFSul", "João Oliveira", 36 ),
+(3, "IFRS", "Felipe de Freitas", 65),
+(4, "Liberato", "Fernanda de Souza", 26),
+(5, "IFSul", "Ana da Silva", 34),
+(6, "Feevale", "Renata Flores", 45),
+(7, "Feevale", "Lucas Bernardes", 29);
+
+INSERT INTO Avaliacao (Id_Avaliacao, Comentario_Geral, Critica_Tecnica, Nota_Conteudo, Nota_Inovacao, Nota_Apresentacao, Restricao, Avaliador_Id, Apresentacao_Id) VALUES
+(1, "Muito bom, mas ta ruim.", "O Juliano não deveria de comportar assim durante a apresentação.", 5, 3, 2, "true", 2 , 1),
+(2, "Não gostei 5 estrelas.", "Premiação não tem dois acentos.", 4, 2, 4, "false", 5 , 1),
+(3, "Niiiiiiiiiiiiice.", "Pense melhor na funcionalidade da próxima vez.", 2, 4, 5, "true", 6, 1),
+(4, "Ta se loco!? bom trabalho.", "O trabalho não tinha o título na capa", 1, 5, 3, "false", 3, 1),
+
+(5, "permanecendo essencialmente inalterado. Se popularizou", "um professor de latim do Hampden-Sydney ", 3, 5 , 2, "true", 7 , 2),
+(6, "de teoria da ética muito popular", "Você usou palavras aleatórias que não parecem ter sido escritas por você", 5, 3, 3, "false", 2 , 2),
+
+(7, "Lorem Ipsum como texto-modelo padrão.", "Proin elementum scelerisque ante ut luctus. Quisque quis leo id lorem sollicitudin tincidunt at a enim..", 2, 2, 3, "true", 4, 3),
+(8, "precisa ter certeza de que não há algo embaraçoso", "para fazer um livro de modelos de tipos.", 5, 4, 4, "true", 3, 1),
+(9, "Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC", "Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito escondido no meio do texto..", 4, 3, 2, "false", 5 , 3),
+
+(10, "cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado", "Aenean quis est vel massa dignissim molestie. Cras sollicitudin dignissim est sed iaculis. ", 4, 2, 4, "false", 5 , 4),
+(11, "Vestibulum aliquet mauris et feugiat accumsan.", "Pense melhor na funcionalidade da próxima vez.", 4, 4, 5, "true", 2, 4),
+(12, "Duis quis purus velit. Ut vehicula tincidunt purus et sodales. ", "Pellentesque risus nunc, vehicula eu nisl iaculis, fermentum posuere massa. Sed condimentum hendrerit venenatis.", 4, 1, 2, "false", 1, 4),
+(13, "Nunc volutpat fermentum molestie. Nulla facilisi.", "Nam eget lorem laoreet, pharetra mauris non, efficitur lectus. ", 3, 5, 4, "true", 6, 4),
+(14, "Nulla facilisi. Nunc ut tincidunt sapien, sed consectetur lectus. ", "Etiam dapibus est eget vestibulum lobortis. ", 4, 3, 5, "false", 7, 4);
+
+ALTER TABLE Autor ADD COLUMN Email VARCHAR (100) NOT NULL;
+
+ALTER TABLE Avaliacao DROP COLUMN Restricao;
+ALTER TABLE Avaliacao  ADD COLUMN Restricao BOOL DEFAULT FALSE;
+ALTER TABLE Autor DROP COLUMN Genero;
