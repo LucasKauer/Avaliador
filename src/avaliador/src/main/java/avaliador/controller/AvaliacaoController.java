84550e28f77654def7a50e7b49b6dc8d93d71ec1
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2015-06-17 16:11:43 -0300

    fix(AvaliacaoController) - Adicionando uma model na controller de cadastro-avaliacao - Implementando um método que faz o controlle da busca de avaliações no banco.

--- src/avaliador/src/main/java/avaliador/controller/AvaliacaoController.java
+++ src/avaliador/src/main/java/avaliador/controller/AvaliacaoController.java
@@ -3,9 +3,11 @@ package avaliador.controller;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import avaliador.dao.ApresentacaoDao;
 import avaliador.dao.AvaliacaoDao;
 import avaliador.model.Avaliacao;
 
@@ -14,15 +16,25 @@ public class AvaliacaoController {
 	
 	@Inject
 	AvaliacaoDao avaliacaoDao;
+			
+	@Inject
+	ApresentacaoDao apresentacaoDao;
 	
 	@RequestMapping(value = "/cadastro-avaliacao", method = RequestMethod.GET)
-	public String cadastrarAvaliacao() {
+	public String cadastrarAvaliacao(Model model) {
+		model.addAttribute("listaApresentacoes", apresentacaoDao.consultaApresentacoes());
 		return "cadastro-avaliacao";
 	}
 	
 	@RequestMapping(value = "/salvar-avaliacao", method = RequestMethod.POST)
 	public String salvarAvaliacao(Avaliacao avaliacao) {
 		avaliacaoDao.inserirAvaliacao(avaliacao);
-		return "redirect:/index";
+		return "cadastro-avaliacao";
+	}
+	
+	@RequestMapping(value = "/busca-avaliacao", method = RequestMethod.POST)
+	public String buscaAvaliacao(int idApresentacao) {
+		avaliacaoDao.buscaAvaliacaoPassandoIdApresentacao(idApresentacao);
+		return "cadastro-avaliacao";
 	}
 }
